b0d7466af9100f54fbebd9d8becf3d566a99d1bc
imath <math.viet@gmail.com>
2014-03-30 23:07:47 +0200

    Edit the js file and remove bp-example-cssjs.php
    As we are hooking bp_enqueue_script from the Skeleton loader class, bp-example-cssjs.php is no more needed.

--- includes/js/general.js
+++ includes/js/general.js
@@ -1,7 +1,8 @@
-jQuery(document).ready( function() {
+( function( $ ) {
+
 	// Put your JS in here, and it will run after the DOM has loaded.
 	
-	// jQuery.post( ajaxurl, {
+	// $.post( ajaxurl, {
 	// 	action: 'my_example_action',
 	// 	'cookie': encodeURIComponent(document.cookie),
 	// 	'parameter_1': 'some_value'
@@ -10,4 +11,6 @@ jQuery(document).ready( function() {
 	// 	... 
 	// } );
 
-});
+	$( '#bp-example-js' ).html( 'Javascript is loaded' );
+
+} )( jQuery );
